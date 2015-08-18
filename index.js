ac2906b1d309cffe9177e49b423fba902c6a479a
Howard Roark <howie@codingcoop.org>
2015-08-17 21:52:42 -0400

    Adding opitional to set a conditional check before validating.

--- index.js
+++ index.js
@@ -25,15 +25,26 @@
         $inputs.each(function(index, element) {
             var $el = $(this);
             $el.removeClass("validation-error");
+
+            if($el.data('validate-conditional')) {
+                var conditional = $($el.data('validate-conditional'));
+
+                if(!conditional.is(":checked")) {
+                    return;
+                }
+            }
+
             if($el.data('validate-required')) {
                 if($el.val() == "") {
                     $el.addClass("validation-error");
                     errors.push($el.data('validate-required-msg'));
                 }
             }
+
             if($el.data('validate-format')) {
                 var val = $el.val();
                 var regex = new RegExp($el.data('validate-format'), 'i');
+
                 if(!val.match(regex)) {
                     $el.addClass("validation-error");
                     errors.push($el.data('validate-format-msg'));
